b96ad51527d627373faa467c682b825ad2ae8798
Till Klocke <till.klocke@gmail.com>
2020-02-11 15:45:41 +0100

    Added preprocessor constants for the length of public key and signature as these are static and used throughout different calculations

--- include/smolcert.h
+++ include/smolcert.h
@@ -5,6 +5,9 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+#define SMOLCERT_PUB_KEY_LEN 32
+#define SMOLCERT_SIGNATURE_LEN 64
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -14,6 +17,9 @@ typedef enum sc_error {
   Sc_Unknown_Error,
 
   Sc_Invalid_Format = 128,
+
+  Sc_Validation_Error = 256,
+  Sc_Invalid_Signature,
 } sc_error_t;
 
 typedef struct sc_validity {
@@ -34,14 +40,16 @@ typedef struct smolcert {
   size_t issuer_len;
   char* subject;
   size_t subject_len;
-  uint8_t public_key[32];
-  uint8_t signature[64];
+  uint8_t public_key[SMOLCERT_PUB_KEY_LEN];
+  uint8_t signature[SMOLCERT_SIGNATURE_LEN];
   sc_validity_t validity;
   sc_extension_t* extensions;
   size_t extensions_len;
 } smolcert_t;
 
-sc_error_t sc_parse_certificate(const uint8_t *buffer, size_t size, smolcert_t* cert);
+sc_error_t sc_parse_certificate(const uint8_t* buffer, size_t size, smolcert_t* cert);
+
+sc_error_t sc_validate_certificate_signature(uint8_t* cert_buf, size_t cert_len, uint8_t* pub_key);
 
 void sc_free_cert(smolcert_t* cert);
 
