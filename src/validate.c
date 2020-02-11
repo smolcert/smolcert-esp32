990ddd85227a9499d6f641c53c2390749d32f7b6
Till Klocke <till.klocke@gmail.com>
2020-02-11 15:57:01 +0100

    Added first functioning (but dirty) signature validation

---
+++ src/validate.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <sodium.h>
+
+#include "smolcert.h"
+
+sc_error_t sc_validate_certificate_signature(uint8_t* cert_buf, size_t cert_len, uint8_t* pub_key) {
+  if (cert_len < SMOLCERT_SIGNATURE_LEN + 3) {
+    return Sc_Invalid_Format;
+  }
+  size_t signature_tag_pos = cert_len - (SMOLCERT_SIGNATURE_LEN + 2);
+  size_t unsigned_cert_len = cert_len - (SMOLCERT_SIGNATURE_LEN + 1);
+  
+  if (cert_buf[signature_tag_pos] != 0x58) {
+    return Sc_Invalid_Format;
+  }
+  // Replace the original byte string tag 0x58 0x40 (byte string, length 64) with a simple value
+  // (major type 7) and a value of 22, signalling Null
+  cert_buf[signature_tag_pos] = (uint8_t)0xF6;
+
+  // Ensure that the last bytes have the necessary length for a signature
+  if ((cert_len - (signature_tag_pos + 2)) != SMOLCERT_SIGNATURE_LEN) {
+    return Sc_Invalid_Format;
+  }
+  
+  if (crypto_sign_verify_detached((unsigned char*)&cert_buf[signature_tag_pos + 2],
+    (unsigned char*)cert_buf, unsigned_cert_len, (unsigned char*)pub_key) != 0) {
+   return Sc_Invalid_Signature;
+  }
+  // Replace the original byte string tag
+  cert_buf[signature_tag_pos] = (uint8_t)0x58;
+  cert_buf[signature_tag_pos +1] = (uint8_t)0x40;
+  return Sc_No_Error;
+}
--- test/TestParsing.c
+++ test/TestParsing.c
@@ -3,8 +3,11 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
-const uint8_t expected_cert_bytes[] = {
+#include <sodium.h>
+
+const uint8_t expected_cert_bytes_without_extension[] = {
         0x87, 0x0c, 0x67, 0x63, 0x6f, 0x6e, 0x6e, 0x63, 0x74, 0x64, 0x82, 0x1a, 0x5d, 0xf0, 0x2e,
         0xf1, 0x1a, 0x5d, 0xf1, 0x80, 0x71, 0x67, 0x63, 0x6f, 0x6e, 0x6e, 0x63, 0x74, 0x64, 0x58,
         0x20, 0x95, 0x38, 0xee, 0xf6, 0x5d, 0x12, 0x34, 0xa6, 0x37, 0x33, 0x45, 0x13, 0x18, 0x06,
@@ -16,11 +19,13 @@ const uint8_t expected_cert_bytes[] = {
         0xa8, 0xfc, 0xd8, 0x29, 0x57, 0x90, 0x0c, 0xf7, 0x82, 0x01};
 
 void test_Parsing_valid_smolcert(void);
+void test_ValidateCertificateSignature(void);
 
 void test_Parsing_valid_smolcert(void) {
   smolcert_t* cert = (smolcert_t*)malloc(sizeof(smolcert_t));
 
-  sc_error_t sc_err = sc_parse_certificate((const uint8_t *)&expected_cert_bytes, sizeof(expected_cert_bytes), cert);
+  sc_error_t sc_err = sc_parse_certificate((const uint8_t *)&expected_cert_bytes_without_extension, 
+    sizeof(expected_cert_bytes_without_extension), cert);
   TEST_ASSERT_EQUAL(Sc_No_Error, sc_err);
 
   TEST_ASSERT_EQUAL_UINT64(12, cert->serial_number);
@@ -28,12 +33,41 @@ void test_Parsing_valid_smolcert(void) {
   TEST_ASSERT_EQUAL_STRING("connctd", cert->subject);
   TEST_ASSERT_EQUAL_UINT64(1576108145, cert->validity.not_after);
   TEST_ASSERT_EQUAL_UINT64(1576021745, cert->validity.not_before);
+  const uint8_t expected_pub_key[32] = {0x95, 0x38, 0xEE, 0xF6, 0x5D, 0x12, 0x34, 0xA6,
+      0x37, 0x33, 0x45, 0x13, 0x18, 0x06, 0xF8, 0x00, 0x6C, 0x4C, 0x6C, 0x81,
+      0xC8, 0xDB, 0x58, 0x19, 0x24, 0x18, 0x9F, 0x82, 0x89, 0xDD, 0x7C, 0x43};
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_pub_key, cert->public_key, 32);
 
   sc_free_cert(cert);
 }
 
+void test_ValidateCertificateSignature(void) {
+  smolcert_t* cert = (smolcert_t*)malloc(sizeof(smolcert_t));
+
+  sc_error_t sc_err = sc_parse_certificate((const uint8_t *)&expected_cert_bytes_without_extension,
+    sizeof(expected_cert_bytes_without_extension), cert);
+  TEST_ASSERT_EQUAL(Sc_No_Error, sc_err);
+
+  // We can't use the const array expected_cert_bytes here, since sc_validate_certificate_signature needs
+  // to temporarely modify the array. So we copy it to a new location
+  uint8_t* cert_bytes = (uint8_t*)malloc(sizeof(expected_cert_bytes_without_extension));
+  memcpy(cert_bytes, expected_cert_bytes_without_extension, sizeof(expected_cert_bytes_without_extension));
+  sc_err = sc_validate_certificate_signature(cert_bytes, sizeof(expected_cert_bytes_without_extension), cert->public_key);
+  TEST_ASSERT_EQUAL(Sc_No_Error, sc_err);
+
+  // Ensure that the validate_signature method did not alter the original buffer
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_cert_bytes_without_extension, cert_bytes, sizeof(expected_cert_bytes_without_extension));
+
+  free(cert_bytes);
+  sc_free_cert(cert);
+}
+
 int main(void) {
+    if (sodium_init() == -1) {
+      return 1;
+    }
     UNITY_BEGIN();
     RUN_TEST(test_Parsing_valid_smolcert);
+    RUN_TEST(test_ValidateCertificateSignature);
     return UNITY_END();
 }
